560a493a8c60368d360bcbb438b7b51c7ae3be57
Chertopolokhov Alexey <leshii26@yandex.ru>
2022-11-05 00:53:57 +0300

    Edits are done

--- src/PersonSurnameComparator.java
+++ src/PersonSurnameComparator.java
@@ -9,8 +9,8 @@ public class PersonSurnameComparator implements Comparator<Person> {
 
     @Override
     public int compare(Person o2, Person o1) {
-        int first = o1.setSurname().split(" ").length;
-        int second = o2.setSurname().split(" ").length;
+        int first = o1.setSurname().split("\\P{IsAlphabetic}+").length;
+        int second = o2.setSurname().split("\\P{IsAlphabetic}+").length;
         int result = first >= size && second >= size? 0: Integer.compare(first, second);
         return result == 0 ? o1.compareTo(o2): result;
     }
